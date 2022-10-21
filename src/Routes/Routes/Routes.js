89443fcbe0b25fcec850f31522896f47efd11f32
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-21 22:54:16 +0600

    components created and route setup with components done

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,19 +1,27 @@
-import { createBrowserRouter } from "react-router-dom";
-import Main from "../../layout/Main";
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../layout/Main';
+import Home from '../../Pages/Home/Home';
+import Category from '../../Pages/Category/Category/Category';
+import News from '../../Pages/News/News/News';
 
 export const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
 
-{
-    path:'/',
-    element:<Main></Main>,
-    children:[
-        {
-            
-        }
-    ] 
-}
-
-
-
-
-])
+      {
+        path: '/category/:id',
+        element: <Category></Category>,
+      },
+      {
+        path: '/news/:id',
+        element: <News></News>,
+      },
+    ],
+  },
+]);
