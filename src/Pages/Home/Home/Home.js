9139cf3ce7af678e701559daf3bcc9db07329c7d
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-24 01:12:36 +0600

    news portal done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
 
-const Category = () => {
-    const categoryNews = useLoaderData();
+const Home = () => {
+    const allNews = useLoaderData();
     return (
         <div>
-            <h2>This is Category has news: {categoryNews.length}</h2>
+            <h2>Dragon News Home: {allNews.length}</h2>
             {
-                categoryNews.map(news =><NewsSummaryCard
+                allNews.map(news => <NewsSummaryCard
                     key={news._id}
                     news={news}
                 ></NewsSummaryCard>)
@@ -17,4 +17,4 @@ const Category = () => {
     );
 };
 
-export default Category;
+export default Home;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <div>
-      <p>
-        <small>Copyright coming soon</small>
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <p><small>Copyright coming soon</small></p>
+        </div>
+    );
 };
 
 export default Footer;
