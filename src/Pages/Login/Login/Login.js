4008845408f15f65eb7545110c47e0ff6141b8d2
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-28 13:41:33 +0600

    dynamic title added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -3,49 +3,56 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
-
+import useTitle from '../../../hooks/useTitle';
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useTitle('Login');
 
-    const {signIn} = useContext(AuthContext);
-    const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        navigate('/');
+      })
+      .catch((error) => console.error(error));
+  };
 
-    const handleSubmit = event =>{
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        signIn(email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-            form.reset();
-            navigate('/')
-        })
-        .catch(error => console.error(error))
-    }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          name="email"
+          type="email"
+          placeholder="Enter email"
+          required
+        />
+      </Form.Group>
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control name="email" type="email" placeholder="Enter email" required />
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+      </Form.Group>
 
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control name="password" type="password" placeholder="Password" required />
-            </Form.Group>
-            
-            <Button variant="primary" type="submit">
-                Login
-            </Button>
-            <Form.Text className="text-danger">
-                
-            </Form.Text>
-        </Form>
-    );
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+      <Form.Text className="text-danger"></Form.Text>
+    </Form>
+  );
 };
 
 export default Login;
